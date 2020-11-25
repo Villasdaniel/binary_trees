edcb409f1cfc32b0b0c610e2e3de2b67b32e799b
Daniel Villa <2057@holbertonschool.com>
2020-11-25 13:44:07 -0500

    dos

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    if (!value)
+        return (NULL);
+	if (parent)
+    {
+        if (parent->left)
+        {
+            new_node = malloc(sizeof(binary_tree_t));
+            if (!new_node)
+                return (NULL);
+            new_node->parent = parent->left->parent;
+            new_node->left = parent->left;
+            new_node->n = value;
+            parent->left->parent = new_node;
+            parent->left = new_node;
+            return (new_node);
+        }
+        else
+        {
+            new_node = malloc(sizeof(binary_tree_t));
+            if (!new_node)
+                return (NULL);
+            new_node->parent = parent;
+            new_node->n = value;
+            new_node->left = NULL;
+            parent->left = new_node;
+            return (new_node);
+        }
+    }
+    else
+        return (NULL);
+}
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    if (!value)
+        return (NULL);
+    if (parent)
+    {
+        if (parent->right)
+        {
+            new_node = malloc(sizeof(binary_tree_t));
+            if (!new_node)
+                return (NULL);
+            new_node->parent = parent->right->parent;
+            new_node->right = parent->right;
+            new_node->n = value;
+            parent->right->parent = new_node;
+            parent->right = new_node;
+            return (new_node);
+        }
+        else
+        {
+            new_node = malloc(sizeof(binary_tree_t));
+            if (!new_node)
+                return (NULL);
+            new_node->parent = parent;
+            new_node->n = value;
+            new_node->right = NULL;
+            parent->right = new_node;
+            return (new_node);
+        }
+    }
+    else
+        return (NULL);
+}
