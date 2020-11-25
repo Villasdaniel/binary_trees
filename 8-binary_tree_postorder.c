ed845578abeac176f22c639af7565d8d12daf624
Daniel Villa <2057@holbertonschool.com>
2020-11-25 17:36:17 -0500

    fix eight

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder -  goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using post-order traversa
  * @tree: node
  * @func: function to print
  * Return: Always 0 (Success)
